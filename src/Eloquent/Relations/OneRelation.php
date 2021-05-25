c024f9a213b3e6ef24979c2288090623a9c05372
d_umble <dylan@firstignite.com>
2021-05-24 20:06:54 -0400

    remove two test statements

--- src/Eloquent/Relations/OneRelation.php
+++ src/Eloquent/Relations/OneRelation.php
@@ -136,10 +136,8 @@ abstract class OneRelation extends BelongsTo implements RelationInterface
     {
         try {
             if (!$id instanceof Model && !$id instanceof \Illuminate\Support\Collection) {
-                return 'me';
                 $id = $this->modelsFromIds($id);
             } elseif (!is_array($id) && !$id instanceof \Illuminate\Support\Collection) {
-                return 'too';
                 $id = [$id];
             } elseif (!$id[0] instanceof Model) {
                 throw new \ErrorException('First argument must be a model, a primary key, or a list of models.');
