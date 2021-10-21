14a8828c8cd5757b6e943fb11b819c26c1d67369
MatthewLoffredo <mloffredo@luc.edu>
2021-10-21 11:38:06 -0500

    fix createWith multiple MERGE statements

--- src/Query/Grammars/CypherGrammar.php
+++ src/Query/Grammars/CypherGrammar.php
@@ -697,7 +697,7 @@ class CypherGrammar extends Grammar
             // Set the WHERE conditions for the heart of the query.
             $cypher .= ' WHERE '.implode(' AND ', $attachments['wheres']);
             // CREATE the relationships between matched nodes
-            $cypher .= ' MERGE'.implode(', ', $attachments['relations']);
+            $cypher .= ' MERGE'.implode(' MERGE ', $attachments['relations']);
         }
 
         $cypher .= " RETURN $parentNode, ".implode(', ', array_merge($createdIdsToReturn, $attachedIdsToReturn));
